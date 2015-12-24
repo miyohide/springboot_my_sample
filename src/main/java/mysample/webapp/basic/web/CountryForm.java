0be0e6514b74503ab61a3af231df542b1d8ee75e
miyohide <miyohide@gmail.com>
2015-12-24 23:07:46 +0900

    add input form controller

---
+++ src/main/java/mysample/webapp/basic/web/CountryForm.java
@@ -0,0 +1,46 @@
+package mysample.webapp.basic.web;
+
+import lombok.Data;
+import org.hibernate.validator.constraints.NotBlank;
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+@Data
+public class CountryForm {
+	@NotBlank
+	@Size(max = 3, message = "{error.size.max}")
+	private String code;
+	
+	@NotBlank
+	@Size(max = 52, message = "{error.size.max}")
+	private String name;
+	
+	@NotBlank
+	@Pattern(regexp = "^(Asia|Europe|North America|Africa|Oceania|Antarctica|South America)$", message = "{countryListForm.continent.pattern}")
+	private String continent;
+	
+	@NotBlank
+	@Size(max = 26, message = "{error.size.max}")
+	private String region;
+	
+	@NotBlank
+	@Digits(integer=8, fraction=2, message = "{error.digits.integerandfraction}")
+	private String surfaceArea;
+	
+	@NotBlank
+	@Digits(integer=11, fraction=0, message = "{error.digits.integeronly}")
+	private String population;
+	
+	@NotBlank
+	@Size(max = 45, message = "{error.size.max}")
+	private String localName;
+
+	@NotBlank
+	@Size(max = 45, message = "{error.size.max}")
+	private String governmentForm;
+	
+	@NotBlank
+	@Size(max = 2, message = "{error.size.max}")
+	private String code2;
+}
