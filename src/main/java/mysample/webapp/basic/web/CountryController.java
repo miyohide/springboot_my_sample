ca62995f6a710ee878cf6b039996fa9fd64cf612
miyohide <miyohide@gmail.com>
2015-12-25 23:06:23 +0900

    add input -> confirm validation.

--- src/main/java/mysample/webapp/basic/web/CountryController.java
+++ src/main/java/mysample/webapp/basic/web/CountryController.java
@@ -1,19 +1,42 @@
 package mysample.webapp.basic.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import mysample.webapp.basic.config.Constant;
 
 @Controller
 @RequestMapping("/country")  // ベースとなるURL
 public class CountryController {
+	@Autowired
+	private Constant constant;
+	
 	// classに設定したRequestMappingの下に設定するURL
 	@RequestMapping("/input")
-	public String input() {
+	public String input(CountryForm countryForm, Model model) {
+		model.addAttribute("continentList", constant.getCONTINENT_LIST());
 		return "country/input";
 	}
 	
+	@RequestMapping("/input/back")
+	public String inputBack(CountryForm countryForm, RedirectAttributes redirectAttributes) {
+		redirectAttributes.addFlashAttribute("countryForm", countryForm);
+		return "redirect:/country/input";
+	}
+	
 	@RequestMapping("/confirm")
-	public String confirm() {
+	public String confirm(@Validated CountryForm countryForm,
+			 BindingResult bindingResult,
+			 Model model) {
+		if (bindingResult.hasErrors()) {
+			model.addAttribute("continentList", constant.getCONTINENT_LIST());
+			return "country/input";
+		}
 		return "country/confirm";
 	}
 	
