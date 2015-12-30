0fd8378fa137262a053f3be8c3c6cd61211e2cac
miyohide <miyohide@gmail.com>
2015-12-30 22:52:58 +0900

    add CRUD repository.

---
+++ src/main/java/mysample/webapp/basic/service/CountryRepository.java
@@ -0,0 +1,11 @@
+package mysample.webapp.basic.service;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.data.repository.CrudRepository;
+
+import mysample.webapp.basic.domain.Country;
+
+@Repository
+public interface CountryRepository extends CrudRepository<Country, String> {
+
+}
