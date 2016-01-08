c35d303b16df8aa1763fb8232abeeca3b71e5dc2
miyohide <miyohide@gmail.com>
2016-01-08 23:32:59 +0900

    add test

---
+++ src/test/java/mysample/webapp/basic/web/CountryListControllerTest.java
@@ -0,0 +1,42 @@
+package mysample.webapp.basic.web;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import mysample.webapp.basic.Application;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Application.class)
+@WebAppConfiguration
+public class CountryListControllerTest {
+
+	@Autowired
+	private WebApplicationContext context;
+	
+	private MockMvc mvc;
+
+	@Before
+	public void setUp() throws Exception {
+		this.mvc = MockMvcBuilders.webAppContextSetup(this.context).build();
+	}
+
+	@Test
+	public void testIndex() throws Exception {
+		this.mvc.perform(get("/countryList")).andExpect(status().isOk())
+        .andExpect(content().contentType("text/html;charset=UTF-8"))
+        .andExpect(view().name("countryList"))
+        .andExpect(xpath("/html/head/title").string("検索/一覧画面"));
+	}
+
+}
---
+++ src/test/java/mysample/webapp/basic/web/LoginControllerTest.java
@@ -0,0 +1,41 @@
+package mysample.webapp.basic.web;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import mysample.webapp.basic.Application;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Application.class)
+@WebAppConfiguration
+public class LoginControllerTest {
+
+	@Autowired
+	private WebApplicationContext context;
+	
+	private MockMvc mvc;
+	
+	@Before
+	public void setUp() {
+		this.mvc = MockMvcBuilders.webAppContextSetup(this.context).build();
+	}
+
+	@Test
+	public void testIndex() throws Exception {
+		this.mvc.perform(get("/")).andExpect(status().isOk())
+		        .andExpect(content().contentType("text/html;charset=UTF-8"))
+		        .andExpect(view().name("login"));
+	}
+
+}
